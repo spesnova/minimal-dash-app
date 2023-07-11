82b26f54ac5234c52b4e9bb5640ff226a0f16e24
Seigo Uchida <spesnova@gmail.com>
2023-07-11 14:09:11 +0900

    Make dash app accessible from internet

--- app.py
+++ app.py
@@ -22,7 +22,8 @@ def update_graph(value):
 
 @app.server.route("/health")
 def healthcheck():
+  app.logger.info("Healthy")
   return "{status: ok}"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
